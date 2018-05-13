a39b42ed8f6067fe225b5af0f1f466f96d760e9e
JefersonLopes2018 <37845180+JefersonLopes2018@users.noreply.github.com>
2018-05-13 00:54:55 -0300

    Update main.py

--- main.py
+++ main.py
@@ -88,6 +88,13 @@ async def on_message(message):
         #sistema_de_enquete
 
 
+     if message.content.lower().startswith("!enquete"):
+      menssagem = message.content[9:]
+      msp = await client.send_message(message.channel, menssagem)
+
+      await client.delete_message(message)
+      await client.add_reaction(msp, ':sim:444562578639945728')
+      await client.add_reaction(msp, ':nao:444562647799562261')
    
 
      if message.content.lower().startswith('?dance'):
