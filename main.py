169836bd944dab70b8f2e7c5eb15fda88de84956
JefersonLopes2018 <37845180+JefersonLopes2018@users.noreply.github.com>
2018-05-19 21:14:58 -0300

    Update main.py

--- main.py
+++ main.py
@@ -271,11 +271,10 @@ async def on_message(message):
      elif message.content.lower().startswith('?staff'):
          canal= client.get_channel("425150435725279253")
          avisos= client.get_channel("392711722172940298")
+         await client.add_reaction(message, ':sim:444562578639945728')
+         await client.send_message(canal, "<@334359138110799872>")
+         await client.send_message(canal, message.content[12:])
          
-          
-          await client.send_message(canal, "<@334359138110799872>")
-          await client.send_message(canal, message.content[12:])
-          await client.add_reaction(message, ':sim:444562578639945728')
         
 
 
