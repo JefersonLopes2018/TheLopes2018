cc4f5491a1e40807ef00923fdd69cabc1708e09e
JefersonLopes2018 <37845180+JefersonLopes2018@users.noreply.github.com>
2018-07-01 16:42:06 -0300

    Update main.py

--- main.py
+++ main.py
@@ -276,7 +276,7 @@ async def on_message(message):
          foi=await client.send_message(message.channel, "```Mensagem Enviada com Sucesso!```")
          await client.add_reaction(foi, ':sim:444562578639945728')
          await client.send_message(canal, sos1) 
-         await client.send_message(canal, "Menssagem:)
+         
          await client.send_message(canal, message.content[7:])
          
         
