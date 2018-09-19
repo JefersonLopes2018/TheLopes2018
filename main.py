92dfef160e476bf2f25c0768dac12af0805fd6d9
JefersonLopes2018 <37845180+JefersonLopes2018@users.noreply.github.com>
2018-09-19 15:50:12 -0300

    Update main.py

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ async def on_member_join(member):
   regras = client.get_channel("424026852097654794")
   bots = client.get_channel("392697933562118144")
   msg = "Bem Vindo {}\n leia as {}".format(member.mention, regras.mention)
-  msg2 = "Olá {},Esse é o canal de Chat, Para liberar Outras salas digite ?perfil em {}".format(member.mention, bots.mention) 
+  msg2 = "Olá {},Esse é o canal de Chat, Para liberar Outras salas digite ** ``?perfil`` ** em {}".format(member.mention, bots.mention) 
   await client.send_message(member, msg) #substitua canal por member para enviar a msg no DM do membro
   await client.send_message(canal, msg2)
 @client.event
@@ -246,7 +246,7 @@ async def on_message(message):
      if message.content.lower().startswith('jarvis'):
        staff = client.get_channel("425150435725279253")
        sos = '<@&463052822175285268> vocês foram solitados por {}'.format(message.author.mention)
-       javai = '{} Vou analisar sua duvida e lhe responder assim que possivel!'.format(message.author.mention)
+       javai = '{} Vou analisar sua duvida e lhe responder assim que possivel!'.format(avatarembed.set_author(name=message.author.name)
        await client.send_message(staff, sos)
        
        await client.send_message(staff, message.content[7:])
