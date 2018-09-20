88cfeb90f9a95803ade77bb47f943b723c9c6b42
JefersonLopes2018 <37845180+JefersonLopes2018@users.noreply.github.com>
2018-09-19 22:47:50 -0300

    Update main.py

--- main.py
+++ main.py
@@ -246,7 +246,7 @@ async def on_message(message):
 
      if message.content.lower().startswith('jarvis'):
        staff = client.get_channel("425150435725279253")
-       sos = '<@&463052822175285268> vocês foram solitados por **{}**,em **{}**'.format(message.author.mention,message.server.name)
+       sos = '<@&463052822175285268> vocês foram solitados por {},em **{}**'.format(message.author.mention,message.server.name)
        javai = '{} Vou analisar sua duvida e lhe responder assim que possivel!'.format(message.author.mention)
        await client.send_message(staff, sos)
        
@@ -295,12 +295,13 @@ async def on_message(message):
          canal= client.get_channel("425150435725279253")
          avisos= client.get_channel("392711722172940298")
          sos1 = '<@&425144881049239553> O usuário {} Mandou uma menssagem de **{}** para o **F SOCIETY**'.format(message.author.mention,message.server.name)
+         CON = '```{}```'.format(message.content[7:])
          await client.delete_message(message)
          foi=await client.send_message(message.channel, "```Mensagem Enviada com Sucesso!```")
          await client.add_reaction(foi, ':sim:444562578639945728')
          await client.send_message(canal, sos1) 
          
-         await client.send_message(canal, message.content[7:])
+         await client.send_message(canal, CON)
          
         
 
