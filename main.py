59b6de9e16ece543eb11caae69e06373607fd20e
JefersonLopes2018 <37845180+JefersonLopes2018@users.noreply.github.com>
2019-02-09 23:33:33 -0200

    Update main.py

--- main.py
+++ main.py
@@ -339,13 +339,7 @@ async def on_message(message):
       await client.send_message(message.channel, "✔ Seu cargo foi adicionado com Sucesso!")
       await client.delete_message(message)
 
-    if message.content.lower().startswith("addapex"):
-      if not message.server.id == '343227251501957121':
-         return await client.send_message(message.channel, "😬**Esse comando é privado!**")
-      cargo8 = discord.utils.get(message.server.roles, name="Apex")
-      await client.add_roles(message.author, cargo8)
-      await client.send_message(message.channel, "✔ Seu cargo foi adicionado com Sucesso!")
-      await client.delete_message(message)
+    
 
     if message.content.lower().startswith("addoverwath"):
       if not message.server.id == '343227251501957121':
