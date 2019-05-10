b0c7ef137d457bf2d4071a814b6bf71aeaefb3bc
JefersonLopes2018 <37845180+JefersonLopes2018@users.noreply.github.com>
2019-05-10 19:54:46 -0300

    Update main.py

--- main.py
+++ main.py
@@ -374,12 +374,13 @@ async def on_message(message):
       if not message.server.id == '343227251501957121':
         return await client.send_message(message.channel, "😬**Esse comando é privado!**")
       senha =  await client.send_message(message.channel, "```Digite a Senha```")
-      await client.delete_message(senha)
       msg1 = await client.wait_for_message(author=message.author, content='1212')
+      await client.delete_message(message) 
+      await client.delete_message(senha)
       cargosjn = discord.utils.get(message.server.roles, name="SJN")
       await client.add_roles(message.author, cargosjn)
       await client.send_message(message.channel, "✔ Seu cargo foi adicionado com Sucesso!")
-      await client.delete_message(message)    
+         
         
         #contato
         
